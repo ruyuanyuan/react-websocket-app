65b4488e3adef7ec9409a00b851d3bb01257a68d
ruyuanyuan <ruyuanyuan@aspiration-cn.com>
2018-03-06 19:52:55 +0800

    redux test

--- fex/src/index.js
+++ fex/src/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux'
+//1 新建store
+//通过 reducer建立
+//根据老的state和action 生成新的state
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+function counter(state=0,action){
+    switch(action.type){
+        case '加机关枪':
+            return state+1
+        case '减机关枪':
+            return state-1
+        default:    
+            return 10        
+    }
+}
+//新建store
+const store=createStore(counter)
+
+const init =store.getState()
+function listener(){
+    const current=store.getState()
+    console.log(`现有机关枪${current}把`)
+}
+store.subscribe(listener)
+
+console.log(init)
+//派发事件 传递action
+store.dispatch({type:'加机关枪'})
+console.log(store.getState())
+store.dispatch({type:'减机关枪'})
+console.log(store.getState())
