ea37f937bf129a4ad401a5ad435b0b356cb89b5d
ruyuanyuan <ruyuanyuan@aspiration-cn.com>
2018-03-06 18:40:06 +0800

    antd-mobile  test

--- fex/src/App.js
+++ fex/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Button} from 'antd-mobile'
 import logo from './logo.svg';
 import './App.css';
 import './es6'
@@ -12,6 +13,8 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <Button>antdMobile</Button> 
+      
         </p>
       </div>
     );
